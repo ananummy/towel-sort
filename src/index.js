2ce6c7af43ce40b1f721076544268396322601ec
Anastasia E <90329657+ananummy@users.noreply.github.com>
2022-07-29 02:58:39 +0300

    fix: change solution

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ module.exports = function towelSort (matrix) {
 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            arr.push(matrix[i][j])
+            let index = i % 2 === 0 ? j : (matrix[i].length - j - 1)
+            arr.push(matrix[i][index])
         }
     }
 
-    return(arr)
+    return arr
 }
