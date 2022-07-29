b51cf74ab515525523d85f0fdd4fcc322ea02d3d
Anastasia E <90329657+ananummy@users.noreply.github.com>
2022-07-29 03:01:15 +0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 
 module.exports = function towelSort (matrix) {
     let arr = []
-
+    
+    if (!matrix) return []
+    
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
             let index = i % 2 === 0 ? j : (matrix[i].length - j - 1)
